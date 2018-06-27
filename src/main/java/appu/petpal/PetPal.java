c6f12538aae6f5bdedd46116acf4ba02ba042f3c
wickersoft <wickersoft@online.de>
2018-06-27 08:04:00 +0200

    Prepared Bukkit plugin skeleton

---
+++ src/main/java/appu/petpal/PetPal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appu.petpal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Dennis
+ */
+public class PetPal extends JavaPlugin {
+    
+}
