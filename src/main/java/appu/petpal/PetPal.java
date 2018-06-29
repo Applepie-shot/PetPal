8b109b569f7937d3a6d067fc46b99a2c3f901a6c
BuildTools <unconfigured@null.spigotmc.org>
2018-06-29 15:35:32 -0500

    Added /setpet and /pethere commands

--- src/main/java/appu/petpal/PetPal.java
+++ src/main/java/appu/petpal/PetPal.java
@@ -5,6 +5,10 @@
  */
 package appu.petpal;
 
+import java.util.HashMap;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -12,5 +16,61 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Dennis
  */
 public class PetPal extends JavaPlugin {
-    
+
+    private static final HashMap<Player, String> newPetNames = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        System.out.println("Appus plugin is super awesome");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equals("setpet")) {
+            if (args.length != 1) {
+                // Player typed in more than one word or nothing
+                // Explain how to use the command
+
+                return true;
+            }
+            if (!(sender instanceof Player)) {
+                // Command came from console
+                // Explain that it can only be used by players
+                
+                return true;
+            }
+            
+            
+
+            //  /setpet doggo
+            //   ^label ^args
+            Player player = (Player) sender;
+            String petName = args[0];
+            newPetNames.put(player, petName);
+            
+            
+        } else if (label.equals("pethere")) {
+            if (args.length != 1) {
+                // Player typed in more than one word or nothing
+                // Explain how to use the command
+
+                return true;
+            }
+            if (!(sender instanceof Player)) {
+                // Command came from console
+                // Explain that it can only be used by players
+                
+                return true;
+            }
+            
+            
+            
+            
+            
+            
+            
+        }
+        return true;
+    }
+
 }
